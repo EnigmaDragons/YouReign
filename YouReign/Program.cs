2d407d4e8debecf1183246d18364243e0204194a
Silas Reinagel <silas.reinagel@gmail.com>
2017-03-16 20:08:48 -0700

    Added EnigmaDragons logo scene

--- YouReign/Program.cs
+++ YouReign/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+using MonoDragons.Core.EngimaDragons;
+using MonoDragons.Core.Engine;
+using MonoDragons.Core.Inputs;
+using MonoDragons.Core.Navigation;
+using YouReign.Scenes;
 
 namespace YouReign
 {
@@ -8,8 +15,25 @@ namespace YouReign
         [STAThread]
         static void Main()
         {
-            //using (var game = new Game1())
-            //    game.Run();
+            using (var game = new MainGame("Logo", new ScreenSettings(1600, 900, false), CreateSceneFactory(), CreateKeyboardController()))
+                game.Run();
+        }
+
+        private static IController CreateKeyboardController()
+        {
+            return new KeyboardController(new Map<Keys, Control>
+            {
+                { Keys.Enter, Control.Start },
+            });
+        }
+
+        private static SceneFactory CreateSceneFactory()
+        {
+            return new SceneFactory(new Dictionary<string, Func<IScene>>
+            {
+                { "Logo", () => new FadingInScene(new LogoScene()) },
+                { "MainMenu", () => new FadingInScene(new MainMenuScene()) },
+            });
         }
     }
 #endif
---
+++ YouReign/Scenes/MainMenuScene.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using MonoDragons.Core.Engine;
+using MonoDragons.Core.Inputs;
+using MonoDragons.Core.UserInterface;
+
+namespace YouReign.Scenes
+{
+    public class MainMenuScene : IScene
+    {
+        public void Init()
+        {
+            Input.ClearBindings();
+            Input.On(Control.Start, () => World.NavigateToScene("CyberRoom1"));
+        }
+
+        public void Update(TimeSpan delta)
+        {
+        }
+
+        public void Draw()
+        {
+            UI.DrawCentered("Images/MainMenu/background", new Vector2(1200, 800));
+            World.DrawRectangle(new Rectangle(0, 0, 1200, 960), Color.FromNonPremultiplied(0, 0, 0, 225));
+            UI.DrawCenteredWithOffset("Images/MainMenu/title", new Vector2(0, -140));
+            UI.DrawCenteredWithOffset("Images/MainMenu/pressenter", new Vector2(0, 200));
+        }
+    }
+}

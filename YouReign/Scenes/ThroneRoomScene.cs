b75c16f8b825248d9329aec3f14c20c6af79c27e
Silas Reinagel <silas.reinagel@gmail.com>
2017-03-16 21:31:21 -0700

    Added Throne Room scene

---
+++ YouReign/Scenes/ThroneRoomScene.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using MonoDragons.Core.Engine;
+using MonoDragons.Core.UserInterface;
+
+namespace YouReign.Scenes
+{
+    public sealed class ThroneRoomScene : IScene
+    {
+        private readonly Vector2 _charPosition = new Vector2(-300, 28);
+
+        public void Init()
+        {
+        }
+
+        public void Update(TimeSpan delta)
+        {
+        }
+
+        public void Draw()
+        {
+            UI.DrawCentered("Images/Backgrounds/throneroom", new Vector2(1600, 900));
+            UI.DrawCenteredWithOffset("Images/UI/portraitbackground", new Vector2(240, 320), _charPosition);
+            UI.DrawCenteredWithOffset("Images/Characters/panickedadvisor", new Vector2(175, 240), _charPosition);
+            UI.DrawCenteredWithOffset("Images/UI/dialoguebox", new Vector2(0, 310));
+        }
+    }
+}
